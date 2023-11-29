3c85fc442085bde518bd5da375b1322bee94a698
anamontiaga <amontiaga@gmail.com>
2023-11-29 23:47:39 +0100

    opacity transition

--- client/src/containers/map/markers/story-markers/marker.tsx
+++ client/src/containers/map/markers/story-markers/marker.tsx
@@ -4,6 +4,8 @@ import { Marker } from 'react-map-gl';
 
 import Image from 'next/image';
 
+import { motion } from 'framer-motion';
+
 import { StoryStepMapMarker } from '@/types/story';
 
 import { Dialog, DialogContent } from '@/components/ui/dialog';
@@ -18,14 +20,17 @@ const MOCK_IMAGES = [
   {
     id: 0,
     img: '/images/mock/carousel.png',
+    legend: 'Summary of GDA Urban EO Information and Use Cases 1',
   },
   {
     id: 1,
     img: '/images/mock/carousel.png',
+    legend: 'Summary of GDA Urban EO Information and Use Cases 2',
   },
   {
     id: 2,
     img: '/images/mock/carousel.png',
+    legend: 'Summary of GDA Urban EO Information and Use Cases 3',
   },
 ];
 
@@ -43,6 +48,18 @@ const StoryMarker = ({ marker: { media, name, id, lat, lng } }: StoryMarkerProps
     setCurrentImage(MOCK_IMAGES[currentImage.id + 1]);
   };
 
+  const variants = {
+    show: {
+      opacity: 1,
+      transition: {
+        ease: 'easeOut',
+        duration: 0.3,
+      },
+    },
+    hide: {
+      opacity: 0,
+    },
+  };
   return (
     <>
       <Marker key={id} longitude={lng} latitude={lat}>
@@ -56,33 +73,67 @@ const StoryMarker = ({ marker: { media, name, id, lat, lng } }: StoryMarkerProps
         </div>
       </Marker>
       <Dialog modal onOpenChange={(open) => setIsFullScreen(open)} open={isFullScreen}>
-        <DialogContent className="flex h-screen w-screen items-center justify-center space-x-14 bg-transparent text-white backdrop-blur-lg">
-          <div className="relative flex h-2/6 w-1/6 items-center">
-            {MOCK_IMAGES[currentImage.id - 1] && (
-              <button onClick={setPrevImage} className="absolute -left-[150px]">
-                <Image
-                  src={MOCK_IMAGES[currentImage.id - 1].img}
-                  height={300}
-                  width={300}
-                  alt="mock"
-                />
-              </button>
-            )}
-          </div>
-          <div className="w-4/6">
-            <Image src={currentImage.img} height={800} width={800} alt="mock" />
-          </div>
-          <div className="relative flex h-2/6 w-1/6 items-center">
-            {MOCK_IMAGES[currentImage.id + 1] && (
-              <button onClick={setNextImage} className="absolute -right-[150px]">
-                <Image
-                  src={MOCK_IMAGES[currentImage.id + 1].img}
-                  height={300}
-                  width={300}
-                  alt="mock"
-                />
-              </button>
-            )}
+        <DialogContent className="flex h-screen w-screen flex-col items-center bg-transparent text-white backdrop-blur-lg">
+          <p className="font-notes mt-10 text-2xl text-white">
+            {currentImage.id + 1} of {MOCK_IMAGES.length}
+          </p>
+          <div className="flex h-full w-screen items-center justify-center">
+            <div className="relative flex h-3/6 w-2/12 items-center">
+              {MOCK_IMAGES[currentImage.id - 1] && (
+                <button onClick={setPrevImage} className="h-full">
+                  <Image
+                    src={MOCK_IMAGES[currentImage.id - 1].img}
+                    style={{
+                      objectFit: 'cover',
+                      height: '100%',
+                      minWidth: '100px',
+                      position: 'absolute',
+                      top: 0,
+                      right: '20%',
+                    }}
+                    height={1200}
+                    width={500}
+                    alt="mock"
+                  />
+                </button>
+              )}
+            </div>
+            <motion.div
+              key={currentImage.id}
+              className="flex w-8/12 flex-col items-center space-y-3"
+              variants={variants}
+              animate={'show'}
+              initial="hide"
+            >
+              <Image src={currentImage.img} height={1000} width={1000} alt="mock" />
+              <p className="font-sans text-sm text-white">{MOCK_IMAGES[currentImage.id].legend}</p>
+            </motion.div>
+            <motion.div
+              className="relative flex h-3/6 w-2/12 items-center"
+              key={currentImage.legend}
+              variants={variants}
+              animate={'show'}
+              initial="hide"
+            >
+              {MOCK_IMAGES[currentImage.id + 1] && (
+                <button onClick={setNextImage} className="h-full">
+                  <Image
+                    src={MOCK_IMAGES[currentImage.id + 1].img}
+                    style={{
+                      objectFit: 'cover',
+                      height: '100%',
+                      minWidth: '100px',
+                      position: 'absolute',
+                      top: 0,
+                      left: '20%',
+                    }}
+                    height={1200}
+                    width={500}
+                    alt="mock"
+                  />
+                </button>
+              )}
+            </motion.div>
           </div>
         </DialogContent>
       </Dialog>
