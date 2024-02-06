e2293527120ed1de9029959ae933c6bf5faa5a39
barbara-chaves <barbarabarcellos90@gmail.com>
2024-02-06 15:00:53 +0100

    Add env variables for mapbox username and style id

--- client/src/constants/map.ts
+++ client/src/constants/map.ts
@@ -1,10 +1,7 @@
 import env from '@/env.mjs';
 
 export const MAPBOX_STYLES = {
-  default:
-    env.NEXT_PUBLIC_ENVIRONMENT === 'production'
-      ? 'mapbox://styles/vizzualityesa/clpb479bt007101o9cmnn1dq9?fresh=true'
-      : 'mapbox://styles/vizzualityesa/clpb479bt007101o9cmnn1dq9?fresh=true',
+  default: `mapbox://styles/${env.NEXT_PUBLIC_MAPBOX_USERNAME}/${env.NEXT_PUBLIC_MAPBOX_STYLE_ID}?fresh=true`,
 };
 
 export const DEFAULT_MAP_STATE = {
