ea60890615583c912b45a7706e98c9349ca4425d
Bárbara Chaves <barbara.chaves@vizzuality.com>
2024-04-03 16:46:08 +0200

    Client/feature/adapt story 1 (#53)
    * Add bubble chart default options
    
    * Fix story content size

--- client/src/components/chart/utils.ts
+++ client/src/components/chart/utils.ts
@@ -1,41 +1,85 @@
 import { MutableRefObject } from 'react';
 
-import { BubbleDataPoint, ChartTypeRegistry, Point } from 'chart.js';
+import { BubbleDataPoint, ChartTypeRegistry, Point, ScriptableContext } from 'chart.js';
 import { ChartJSOrUndefined } from 'react-chartjs-2/dist/types';
 
-export const getChartDefaultOptions = (options: any) => {
+export const PluginCallbacks = {
+  PredictedTravelDemandDhakaTooltopTitle: (context: any) => {
+    return `${context[0].raw.y} in ${context[0].raw.x}`;
+  },
+  PredictedTravelDemandDhakaTooltopLabel: (context: any) => {
+    return `${context.raw.value?.toLocaleString()} ${context.raw.scale}`;
+  },
+};
+
+export const bubbleDefaultOptions = {
+  radius: (context: ScriptableContext<'bubble'>) => {
+    const maxRadius = 20;
+    const minRadius = 10;
+    const max = Math.max(...context.dataset.data.map((d: any) => d.value || 0));
+    const min = Math.min(...context.dataset.data.map((d: any) => d.value || 0));
+    const value = (context.dataset.data[context.dataIndex] as any).value || 0;
+    const normalized = minRadius + ((value - min) * (maxRadius - minRadius)) / (max - min);
+    context.dataset.data[context.dataIndex].r = normalized;
+    return normalized;
+  },
+};
+
+const extractPluginCallbackFunction = (options: Record<string, any>) => {
+  const plugins = options?.plugins || {};
+  if (!plugins) return options;
+  for (const key in plugins) {
+    const plugin = plugins[key];
+    if (plugin?.callbacks) {
+      for (const callback in plugin.callbacks) {
+        const pluginCallback = plugin.callbacks[callback];
+        if (typeof pluginCallback === 'string' && pluginCallback in PluginCallbacks) {
+          plugins[key].callbacks[callback] =
+            PluginCallbacks[pluginCallback as keyof typeof PluginCallbacks];
+        }
+      }
+    }
+  }
+  return {
+    ...options,
+    plugins,
+  };
+};
+
+export const getChartDefaultOptions = (a: unknown) => {
+  const options = !!a && typeof a === 'object' ? extractPluginCallbackFunction(a) : {};
   return {
     borderColor: '#fff',
     interaction: {
       intersect: false,
-      mode: 'index',
-      ...options.interaction,
+      mode: 'point',
+      ...options?.interaction,
     },
     scales: {
       x: {
         grid: {
           display: false,
           color: '#fff',
-          ...options.scales?.x?.grid,
+          ...options?.scales?.x?.grid,
         },
         ticks: {
           maxTicksLimit: 5,
           color: '#fff',
           padding: 10,
-          ...options.scales?.x?.ticks,
+          ...options?.scales?.x?.ticks,
         },
-        ...options.scales?.x,
+        ...options?.scales?.x,
       },
       y: {
         border: {
           dash: [4, 4],
           display: false,
-          ...options.scales?.y?.border,
+          ...(options?.scales?.y as any)?.border,
         },
         grid: {
           color: '#fff',
           drawTicks: false,
-          ...options.scales?.y?.grid,
+          ...options?.scales?.y?.grid,
         },
         ticks: {
           padding: 0,
@@ -47,18 +91,34 @@ export const getChartDefaultOptions = (options: any) => {
             return label;
           },
           maxTicksLimit: 5,
-          ...options.scales?.y?.ticks,
+          ...options?.scales?.y?.ticks,
         },
-        ...options.scales?.y,
+        ...options?.scales?.y,
       },
-      ...options.scales,
+      ...options?.scales,
     },
     plugins: {
       legend: {
         display: false,
-        ...options.plugins?.legend,
+        ...options?.plugins?.legend,
       },
-      ...options.plugins,
+      tooltip: {
+        mode: 'point',
+        bodyFont: {
+          size: 14,
+          weight: 'bold',
+        },
+        bodyColor: '#003247',
+        titleFont: {
+          size: 14,
+          weight: 'bold',
+        },
+        titleColor: '#9AABB5',
+        displayColors: false,
+        backgroundColor: '#fff',
+        ...options?.plugins?.tooltip,
+      },
+      ...options?.plugins,
     },
     ...options,
   };
@@ -74,19 +134,18 @@ export const getChartDefaultData = (
 ) => {
   if (!chartRef.current) return data;
 
-  const gradient = chartRef.current.ctx.createLinearGradient(0, 0, 400, 200);
-  gradient.addColorStop(0, 'rgba(0, 174, 157, 1)');
-  gradient.addColorStop(1, 'rgba(0, 174, 157, 0)');
-  return {
-    ...data,
-    datasets: data.datasets.map((dataset: any) => ({
-      ...dataset,
-      backgroundColor: gradient,
-      borderColor: 'rgb(0, 174, 157)',
-      borderWidth: 2,
-      pointRadius: 0,
-      pointHoverRadius: 0,
-      pointHoverBorderWidth: 0,
-    })),
-  };
+  if (data.datasets?.some((dataset: any) => dataset.backgroundColor === 'GRADIENT')) {
+    const gradient = chartRef.current.ctx.createLinearGradient(0, 0, 400, 200);
+    gradient.addColorStop(0, 'rgba(0, 174, 157, 1)');
+    gradient.addColorStop(1, 'rgba(0, 174, 157, 0)');
+    return {
+      ...data,
+      datasets: data.datasets.map((dataset: any) => ({
+        ...dataset,
+        backgroundColor: dataset.backgroundColor === 'GRADIENT' && gradient,
+      })),
+    };
+  }
+
+  return data;
 };
--- client/src/containers/story/steps/layouts/map-step.tsx
+++ client/src/containers/story/steps/layouts/map-step.tsx
@@ -36,68 +36,64 @@ const MapStepLayout = ({ step, category, showContent, stepIndex }: MapStepLayout
   };
 
   return (
-    <div className="pointer-events-none h-full max-h-screen ">
-      <div className="flex h-full justify-between px-14 pt-[84px]">
-        <div className="flex flex-1 flex-col space-y-8">
-          {!!story_summary?.length && (
-            <div className="pointer-events-auto min-w-fit space-y-4 lg:w-[280px]">
-              <div className={cn(cardClassName, 'space-x-2')}>
-                <CategoryIcon className="inline h-10 w-10 fill-gray-200" slug={category?.slug} />
-                <span className="font-open-sans text-sm">{category?.name}</span>
-              </div>
-
-              <div className={cn(cardClassName, 'space-y-4')}>
-                {story_summary?.map((item) => (
-                  <div className="space-y-1" key={item.id}>
-                    <div className="text-enlight-yellow-400 flex items-center gap-2">
-                      <h2 className="tetx-sm font-bold uppercase">{item.title}</h2>
-                      <Tooltip>
-                        <TooltipTrigger>
-                          <InfoIcon className="h-4 w-4" />
-                        </TooltipTrigger>
-                        <TooltipContent className="max-w-xs">{item.info}</TooltipContent>
-                      </Tooltip>
-                    </div>
-                    <p className="font-open-sans">{item.content}</p>
-                  </div>
-                ))}
-              </div>
+    <div className="flex justify-between">
+      <div className="flex flex-1 flex-col space-y-8 pt-[84px]">
+        {!!story_summary?.length && (
+          <div className="pointer-events-auto min-w-fit space-y-4 lg:w-[280px]">
+            <div className={cn(cardClassName, 'space-x-2')}>
+              <CategoryIcon className="inline h-10 w-10 fill-gray-200" slug={category?.slug} />
+              <span className="font-open-sans text-sm">{category?.name}</span>
             </div>
-          )}
-        </div>
-        <div className="">
-          <div className="flex h-fit min-h-full flex-col items-end justify-center space-y-6 pb-6">
-            {[card, widget]?.map((item, index) => {
-              if (!item) return null;
-              return (
-                <div
-                  key={item?.id}
-                  onClick={handleClickCard}
-                  className={cn(
-                    'pointer-events-auto cursor-pointer overflow-hidden rounded border border-gray-800 bg-[#335e6f] bg-opacity-50 p-8 backdrop-blur transition-all duration-300 ease-in-out',
-                    showContent ? 'opacity-100' : 'opacity-0'
-                  )}
-                >
-                  <div className="w-[400px] space-y-1">
-                    {item?.title && (
-                      <h2 className="font-notes text-2xl font-bold">{item?.title}</h2>
-                    )}
-                    {index === 0 ? (
-                      <div className="font-open-sans space-y-4">
-                        {(item as MapLayerCardComponent)?.content?.split('\n').map((p, i) => (
-                          <p key={i} className="text-sm">
-                            {p}
-                          </p>
-                        ))}
-                      </div>
-                    ) : (
-                      <Chart widget={item as WidgetWidgetComponent} />
-                    )}
+
+            <div className={cn(cardClassName, 'space-y-4')}>
+              {story_summary?.map((item) => (
+                <div className="space-y-1" key={item.id}>
+                  <div className="text-enlight-yellow-400 flex items-center gap-2">
+                    <h2 className="tetx-sm font-bold uppercase">{item.title}</h2>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <InfoIcon className="h-4 w-4" />
+                      </TooltipTrigger>
+                      <TooltipContent className="max-w-xs">{item.info}</TooltipContent>
+                    </Tooltip>
                   </div>
+                  <p className="font-open-sans">{item.content}</p>
                 </div>
-              );
-            })}
+              ))}
+            </div>
           </div>
+        )}
+      </div>
+      <div className="relative min-h-screen pt-[84px]">
+        <div className="flex min-h-full flex-col items-end justify-end space-y-6 pb-6">
+          {[card, widget]?.map((item, index) => {
+            if (!item) return null;
+            return (
+              <div
+                key={item?.id}
+                onClick={handleClickCard}
+                className={cn(
+                  'pointer-events-auto cursor-pointer overflow-hidden rounded border border-gray-800 bg-[#335e6f] bg-opacity-50 p-8 backdrop-blur transition-all duration-300 ease-in-out',
+                  showContent ? 'opacity-100' : 'opacity-0'
+                )}
+              >
+                <div className="w-[400px] space-y-1">
+                  {item?.title && <h2 className="font-notes text-2xl font-bold">{item?.title}</h2>}
+                  {index === 0 ? (
+                    <div className="font-open-sans space-y-4">
+                      {(item as MapLayerCardComponent)?.content?.split('\n').map((p, i) => (
+                        <p key={i} className="text-sm">
+                          {p}
+                        </p>
+                      ))}
+                    </div>
+                  ) : (
+                    <Chart widget={item as WidgetWidgetComponent} />
+                  )}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
