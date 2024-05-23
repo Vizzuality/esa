783f948bb663e42530082da947ee9c454732ba3f
barbara-chaves <barbarabarcellos90@gmail.com>
2024-05-23 10:07:04 +0200

    Fix globe position

--- client/src/containers/map/index.tsx
+++ client/src/containers/map/index.tsx
@@ -210,7 +210,7 @@ export default function MapContainer() {
         dragPan={isHomePage}
         onMapViewStateChange={handleMapViewStateChange}
         className={cn(isHomePage ? 'cursor-pointer' : 'pointer-events-none cursor-default')}
-        padding={{ top: 50, bottom: 50, left: 200, right: 300 }}
+        padding={{ top: 50, bottom: 50, left: 200, right: 200 }}
       >
         {/* <Controls className="absolute right-5 top-12 z-40 sm:right-6 sm:top-6">
           <ZoomControl />
