028f48569b33d92a438e639929a2e3ec2af30ef2
María <maluenarod@gmail.com>
2025-10-21 11:48:05 +0200

    banner update

--- client/src/app/(landing)/layout.tsx
+++ client/src/app/(landing)/layout.tsx
@@ -3,9 +3,12 @@ import Providers from '@/app/(landing)/layout-providers';
 import Banners from '@/containers/banners';
 import Map from '@/containers/map';
 
+import WarningBanner from '@/components/warning-banner';
+
 export default function LandingLayout({ children }: { children: React.ReactNode }) {
   return (
     <Providers>
+      <WarningBanner />
       <Banners />
       <Map />
 
