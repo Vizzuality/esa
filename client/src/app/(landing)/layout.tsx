4b1b03b5031b8525e1d157bd618b4c8da89815df
María <maluenarod@gmail.com>
2025-10-22 13:45:00 +0200

    remove banner

--- client/src/app/(landing)/layout.tsx
+++ client/src/app/(landing)/layout.tsx
@@ -3,12 +3,13 @@ import Providers from '@/app/(landing)/layout-providers';
 import Banners from '@/containers/banners';
 import Map from '@/containers/map';
 
-import WarningBanner from '@/components/warning-banner';
+// Activate banner when experiende production issues that can't be solved quickly
+// import WarningBanner from '@/components/warning-banner';
 
 export default function LandingLayout({ children }: { children: React.ReactNode }) {
   return (
     <Providers>
-      <WarningBanner />
+      {/* <WarningBanner /> */}
       <Banners />
       <Map />
 
