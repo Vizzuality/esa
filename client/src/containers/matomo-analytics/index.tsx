114908149f578c4825bc4b65c14808d9e97f9dd5
María <maluenarod@gmail.com>
2025-10-24 09:28:35 +0200

    env var

--- client/src/containers/matomo-analytics/index.tsx
+++ client/src/containers/matomo-analytics/index.tsx
@@ -5,10 +5,13 @@ import { useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 import { trackAppRouter } from '@socialgouv/matomo-next';
+
+import { env } from '@/env.mjs';
+
 // https://github.com/SocialGouv/matomo-next
 
-const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
-const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+const MATOMO_URL = env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = env.NEXT_PUBLIC_MATOMO_SITE_ID;
 
 export function MatomoAnalytics() {
   const pathname = usePathname();
@@ -18,18 +21,17 @@ export function MatomoAnalytics() {
     if (!MATOMO_URL || !MATOMO_SITE_ID) {
       return;
     }
-
     const urlSearchParams = searchParams
       ? new URLSearchParams(Array.from(searchParams.entries()))
       : new URLSearchParams();
 
     trackAppRouter({
-      url: MATOMO_URL,
-      siteId: MATOMO_SITE_ID,
+      url: MATOMO_URL!,
+      siteId: MATOMO_SITE_ID!,
       pathname,
       searchParams: urlSearchParams,
-      enableHeatmapSessionRecording: true,
-      enableHeartBeatTimer: true,
+      onInitialization: () => console.info('Initializing Matomo'),
+      onScriptLoadingError: () => console.error('Error loading Matomo script'),
     });
   }, [pathname, searchParams]);
   return null;
