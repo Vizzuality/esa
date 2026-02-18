3a061e1a2ffe94457ea30def2bc4cedfa13d13ee
María <maluenarod@gmail.com>
2026-02-18 13:13:04 +0100

    main suspense

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,57 +3,81 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   output: 'standalone',
   transpilePackages: ['@esa/types'],
+
   images: {
-    domains: [
-      'api.mapbox.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.mapbox.com',
+      },
       ...(process.env.NEXT_PUBLIC_API_URL
-        ? [new URL(process.env.NEXT_PUBLIC_API_URL).hostname]
+        ? [
+            {
+              protocol: 'https',
+              hostname: new URL(process.env.NEXT_PUBLIC_API_URL).hostname,
+            },
+          ]
         : []),
-      'esa-gda-comms-staging-cms.fra1.digitaloceanspaces.com',
-      'fra1.digitaloceanspaces.com',
-      'esa-gda-comms-staging-mfafc.ondigitalocean.app',
-      'https://impact-sphere-gda.esa.int',
-      'esa-dev-public.s3.amazonaws.com',
-      'esa-dev-public.s3.eu-west-3.amazonaws.com',
+      {
+        protocol: 'https',
+        hostname: 'esa-gda-comms-staging-cms.fra1.digitaloceanspaces.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fra1.digitaloceanspaces.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'esa-gda-comms-staging-mfafc.ondigitalocean.app',
+      },
+      {
+        protocol: 'https',
+        hostname: 'impact-sphere-gda.esa.int',
+      },
+      {
+        protocol: 'https',
+        hostname: 'esa-dev-public.s3.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'esa-dev-public.s3.eu-west-3.amazonaws.com',
+      },
     ],
   },
+
   env: {
     RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED: 'false',
     NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || '',
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  redirects: async () => {
+
+  typescript: { ignoreBuildErrors: true },
+
+  async redirects() {
     return [
       {
         source: '/',
-        has: [
-          {
-            type: 'cookie',
-            key: 'esa-visited',
-            value: 'true',
-          },
-        ],
+        has: [{ type: 'cookie', key: 'esa-visited', value: 'true' }],
         permanent: false,
         destination: '/globe',
       },
       {
         source: '/',
-        missing: [
-          {
-            type: 'cookie',
-            key: 'esa-visited',
-          },
-        ],
+        missing: [{ type: 'cookie', key: 'esa-visited' }],
         permanent: false,
         destination: '/home',
       },
     ];
   },
+
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -63,18 +87,12 @@ const nextConfig = {
           loader: '@svgr/webpack',
           options: {
             svgoConfig: {
-              plugins: [
-                {
-                  name: 'removeViewBox',
-                  active: false,
-                },
-              ],
+              plugins: [{ name: 'removeViewBox', active: false }],
             },
           },
         },
       ],
     });
-
     return config;
   },
 };
