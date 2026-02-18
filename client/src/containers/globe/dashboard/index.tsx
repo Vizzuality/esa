1cf51e0728b2effd4b89414680aa11f67df13aef
María <maluenarod@gmail.com>
2026-02-18 10:39:38 +0100

    dashboard changes

--- client/src/containers/globe/dashboard/index.tsx
+++ client/src/containers/globe/dashboard/index.tsx
@@ -4,6 +4,8 @@ import GradientLine from '@/components/ui/gradient-line';
 import DashboardNumbers from './numbers';
 import DashboardRegions from './regions';
 
+import { ExternalLinkIcon, X } from 'lucide-react';
+
 const Dashboard = () => {
   return (
     <>
@@ -15,9 +17,33 @@ const Dashboard = () => {
           <GradientLine />
         </div>
       </div>
-      <Card className="max-h-[calc(100%-142px)]" title="Case Studies supporting IFI processes">
-        <DashboardRegions />
-      </Card>
+      <div className="space-y-2">
+        <div className="h-fit">
+          <Card title="Programme Dashboard">
+            <a
+              target="_blank"
+              className="font-open-sans flex justify-between gap-4 px-4 text-sm leading-snug"
+              href="https://gda.esa.int/impact-dashboard/"
+            >
+              Detailed report dashboard on ESA GDA programme.
+              <ExternalLinkIcon className="h-4 w-4 shrink-0" />
+            </a>
+          </Card>
+        </div>
+        <div className="h-fit">
+          <Card title="Knowledge Hub">
+            <a
+              target="_blank"
+              className="font-open-sans flex justify-between gap-4 px-4 text-sm leading-snug"
+              href="https://knowledge-hub-gda.esa.int/"
+            >
+              Extensive and interactive repository of European Earth Observation service
+              capabilities.
+              <ExternalLinkIcon className="h-4 w-4 shrink-0" />
+            </a>
+          </Card>
+        </div>
+      </div>
     </>
   );
 };
